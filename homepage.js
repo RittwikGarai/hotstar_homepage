010430988a4ed14f8d4267a84ba644757e9aec03
rittwikgarai <aryamaan1833@gmail.com>
2022-09-06 20:51:54 +0530

    coupon

--- homepage.js
+++ homepage.js
@@ -3,3 +3,15 @@ let toggle=document.getElementById("mode");
 toggle.addEventListener('click',()=>{
   document.body.classList.toggle('mylight');
 })
+
+function loadCoupon(){
+  document.getElementById('coupon').style.display="block";
+  document.getElementById('cf').style.opacity="0.5";
+  document.getElementById('footer').style.opacity="0.5";
+}
+
+const closeCoupon = () => {
+  document.getElementById('coupon').style.display="none";
+  document.getElementById('cf').style.opacity="1";
+  document.getElementById('footer').style.opacity="1";
+}
