3f10a0e744831c53d6ed5c0dff8973fe65449510
rittwikgarai <aryamaan1833@gmail.com>
2022-09-21 22:15:00 +0530

    LightDark Coupon Weather/Geolocation

--- homepage.js
+++ homepage.js
@@ -10,13 +10,13 @@ toggle.addEventListener('click',()=>{
 function loadCoupon(){
     document.getElementById('coupon').style.display="block";
     document.getElementById('cf').style.opacity="0.5";
-    document.getElementById('footer').style.opacity="0.5";
+    document.getElementById('details').style.opacity="0.5";
   }
   
   const closeCoupon = () => {
     document.getElementById('coupon').style.display="none";
     document.getElementById('cf').style.opacity="1";
-    document.getElementById('footer').style.opacity="1";
+    document.getElementById('details').style.opacity="1";
   }
 
 
