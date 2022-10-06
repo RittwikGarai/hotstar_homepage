6df70588ab3356cb23b4b3aed2d86c2d273fe044
rittwikgarai <aryamaan1833@gmail.com>
2022-10-06 12:18:13 +0530

    data input

---
+++ data/categoery.js
@@ -0,0 +1,31 @@
+[
+    {
+        "type":"Continue Watching",
+        "type_id":1,
+    },
+    {
+        "type":"Latest & Trending",
+        "type_id":2,
+    },
+    {
+        "type":"Popular Shows",
+        "type_id":3,
+    },
+    {
+        "type":"Movies Recommended For You",
+        "type_id":4,
+    },
+    {
+        "type":"Shows Recommended For You",
+        "type_id":5,
+    },
+    {
+        "type":"Popular Movies",
+        "type_id":6,
+    },
+       
+]
+
+
+
+
---
+++ data/continue_watching.js
@@ -0,0 +1,33 @@
+[
+  {
+    "type_id":1,
+    "product_id":1,
+    "product_name":"She HULK ATTORNEY AT LAW",
+    "description":"NEW EPISODE EVERY THURSDAY AT 12:30 PM. Jennifer Walters, an attorney specialising in superhuman-oriented legal cases, must navigate the complicated life of a single, green 6-foot-7-inch hulk.",
+    "product_type":["Action","Comedy","Adventure","SuperHero","Science Fiction","Marvel"],
+    "product_image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.androidhire.com%2Fwp-content%2Fuploads%2F2022%2F07%2Fshe_hulk_poster_by_arifinity_df5pytq-fullview-scaled.jpg&imgrefurl=https%3A%2F%2Fwww.androidhire.com%2Fweb-stories%2Fmarvel-studios-she-hulk-attorney-at-law-trailer-and-poster-released%2F&tbnid=f3aRf7i_b3k4zM&vet=12ahUKEwiexeLT4sr6AhWeA7cAHaGzDJ8QMygDegUIARDSAQ..i&docid=XDapJhbb1Yk3QM&w=1640&h=2048&q=She%20HULK%20ATTORNEY%20AT%20LAW&ved=2ahUKEwiexeLT4sr6AhWeA7cAHaGzDJ8QMygDegUIARDSAQ",
+    "product_url":"https://www.youtube.com/watch?v=gim2kprjL50",
+    "language":["English","Hindi","Tamil","Telegu","Malyalam","Kannada","Marathi"],
+  },
+  {
+    "type_id":1,
+    "product_id":2,
+    "product_name":"HERO",
+    "description":"A gangster's son kidnaps the daughter of the Chief of Police, as her father has some crucial evidence. While the police chase the two, love blooms between them.",
+    "product_type":"Romance",
+    "product_image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2Fe%2Fea%2FHero-2015-Poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FHero_(2015_Hindi_film)&tbnid=JiqhkuwQ13-91M&vet=12ahUKEwis4L7K48r6AhWp9XMBHQ6JAIwQMygAegUIARDjAQ..i&docid=7iehISMMiePk6M&w=250&h=363&q=hero%20movie&ved=2ahUKEwis4L7K48r6AhWp9XMBHQ6JAIwQMygAegUIARDjAQ",
+    "product_url":"https://www.youtube.com/watch?v=6k3wapAOu-M",
+    "language":"Hindi",
+  },
+  {
+    "type_id":1,
+    "product_id":3,
+    "product_name":"VR",
+    "description":"When a series of mysterious events stirs pandemonium in a village, an eccentric cop Vikrant Rona sets out to solve the case wherein everyone is under the radar.",
+    "product_type":["Action","Thriller"],
+    "product_image":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fakamaividz2.zee5.com%2Fimage%2Fupload%2Fw_504%2Ch_756%2Cc_scale%2Cf_webp%2Cq_auto%3Aeco%2Fresources%2F0-0-1z5206542%2Fportrait%2F1920x770dc21179fb962439e91702a6539ea3791.jpg&imgrefurl=https%3A%2F%2Fwww.zee5.com%2Fmovies%2Fdetails%2Fwatch-vikrant-rona-full-hd-movie-online-on-zee5%2F0-0-1z5206541&tbnid=xIMEEtJdCqEZbM&vet=12ahUKEwjJ09Km4cr6AhUwjNgFHR8XA-kQMygBegUIARDIAQ..i&docid=8KlU9VRbBpP3lM&w=504&h=756&itg=1&q=vikrant%20rona&ved=2ahUKEwjJ09Km4cr6AhUwjNgFHR8XA-kQMygBegUIARDIAQ",
+    "product_url":"https://www.youtube.com/watch?v=uEYg1920AAc",
+    "language":["Hindi","Tamil","Telegu"],
+  }
+  
+]
